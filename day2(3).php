5454af4f0fc3a3d032aa2a400e4b54529d6345c2
Dhruv bhanderi <84907347+BhanderiDhruv@users.noreply.github.com>
2021-05-27 16:15:05 +0530

    Add files via upload

---
+++ day2(3).php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+		session_start();
+?>
+<html>
+	<head>
+		<title>day2(3)</title>
+	</head>
+	
+<body>
+	<form action="day2(4).php">
+		<?php
+			echo "Welcome To the second day of the php internship.";
+		?>
+			<br><input type="submit" name="submit" value="logout">
+	</form>
+</body>
+</html>
---
+++ day2.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+	<title>Day 2</title>
+</head>
+<body>
+	<h2 style="float: left; margin:30px 0px 0px 20px;">DhruvBhanderi</h2>
+	<h2 style="float:left; margin: 30px 0px 0px 490px">Day-2</h2>
+	<h2 style="float: right; margin: 30px 20px 0px 0px;">internship</h2><br>
+	<hr width="1320px" color="blue">
+	<center>
+	
+	<form method="POST" action="day2(2).php">
+	<table border="3">
+		<tr><td id="a">Email Id:-</td><td id="b"><input type="email" name="username" required></td></tr>
+
+		<tr><td id="a">Password</td><td id="b"><input type="password" name="pass" required></td></tr>
+		
+		<tr><td  colspan="2" id="c"><input type="submit" name="submit"></td></tr>
+		
+	</table>
+	</form>
+	
+</center>
+</body>
+</html>
